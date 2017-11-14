e8ab750568631f0c611afdb655ee43f68cf32f2a
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-14 17:46:47 +0100

    Added core database connection support

---
+++ admin/core/database/DB.php
@@ -0,0 +1,22 @@
+<?php
+
+class DB
+{
+	
+	public static function CONNECT () {
+		
+		$host = "localhost";
+		$db = "nordicNoir";
+		$user = "root";
+		$password = "";
+		
+		try {
+			$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+			return $pdo;
+			
+		}catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+	
+}
