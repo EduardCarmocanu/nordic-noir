bf76454899d61c55bf1cbfdc08f32566f1910271
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-25 13:32:19 +0100

    Navigation bar changes color based on position on page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     
     var sideShow = false;
     $("#hamburger").addEventListener('click', function () {
-
         if (sideShow){
             $('#sideNav').style.right = '-100vw';
         }else{
@@ -12,5 +11,16 @@ document.addEventListener("DOMContentLoaded", function() {
         sideShow = !sideShow;
     });
 
+    window.onscroll = function () {
+        var secondSection = $('.second-section')[0];
+        var nav = $('.navbar-items-wrapper')[0];
+
+        if (window.scrollY + 35 > secondSection.offsetTop) {
+            nav.style.color = '#171717';
+        }
+        else if (window.scrollY + 35 < secondSection.offsetTop) {
+            nav.style.color = '#f8f8f8';
+        }
+    }
 });
 
