651e3ff25d66db7ad96575e7d675d1a92c4ad5ab
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-26 13:44:17 +0100

    Implemented trigger toggle for the navigation bar | CHECK THE CODE

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
     
-    var sideShow = false;
-    $("#hamburger").addEventListener('click', function () {
-        if (sideShow){
-            $('#sideNav').style.right = '-100vw';
-        }else{
-            $('#sideNav').style.right = '0vw';
+    /*
+    * REMOVED FOR TESTING
+    */
+    
+    // var sideShow = false;
+    // $("#hamburger").addEventListener('click', function () {
+    //     if (sideShow){
+    //         $('#sideNav').style.right = '-100vw';
+    //     }else{
+    //         $('#sideNav').style.right = '0vw';
+    //     }
+    //     sideShow = !sideShow;
+    // });
+
+
+    // Check to see if element exists
+
+    var triggerPoints = $('*[trigger]');
+        nav = $('nav')[0];
+        
+    window.addEventListener('scroll', function () {
+        for (var i = 0; i < triggerPoints.length; i++) {
+            if (window.pageYOffset > triggerPoints[i].offsetTop) {
+                nav.style.backgroundColor = triggerPoints[i].getAttribute('trigger');
+            }
         }
-        sideShow = !sideShow;
     });
-
-    window.onscroll = function () {
-        var secondSection = $('.second-section')[0];
-        var nav = $('.navbar-items-wrapper')[0];
-
-        if (window.scrollY + 35 > secondSection.offsetTop) {
-            nav.style.color = '#171717';
-        }
-        else if (window.scrollY + 35 < secondSection.offsetTop) {
-            nav.style.color = '#f8f8f8';
-        }
-    }
 });
 
