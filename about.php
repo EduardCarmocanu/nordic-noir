19c1528d2ac4d56bcd73434a28cf91bdadb6d068
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-26 15:49:10 +0100

    About page layout

--- about.php
+++ about.php
@@ -13,11 +13,60 @@
 <body>
 	<?php include('partials/navbar.php'); ?>
 	
-	<main>
-		<section>BORGEN</section>
-		<section>THE KILLING</section>
-		<section>THE BRIDGE</section>
-		<section>PRIVATE TOURS</section>
+	<main class="pt-5 mt-1" trigger="black">
+		<section class="separator-image full-width half-view-height mb-3" style="background-color: rgb(40, 40, 40)">
+			<img src="#" alt="separator parallax image">
+		</section>
+
+		<div class="heading pl-5 pt-2">
+			<h1>ABOUT US</h1>
+		</div>
+		<br>
+		<section class="about-section flex flex-jusitify-center pl-4 pr-4">
+			
+			<div class="pa-1">
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum numquam fugiat, eaque temporibus distinctio dolores, eos dolorum quisquam voluptates accusamus sapiente rerum doloremque tempora asperiores autem commodi velit sint. Et.</p>
+				<br>
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quisquam incidunt odit vitae fugit</p>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore atque ipsam sequi nam fugit voluptatem accusantium, ut enim, aperiam minus velit non nostrum optio ratione, dolorum totam eligendi earum ducimus?</p>
+			</div>
+			<div class="pa-1">
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum numquam fugiat, eaque temporibus distinctio dolores, eos dolorum quisquam voluptates accusamus sapiente rerum doloremque tempora asperiores autem commodi velit sint. Et.</p>
+				<br>
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quisquam incidunt odit vitae fugit</p>
+			</div>
+		</section>
+		
+		
+		<h1 class="text-center pt-5">MEET OUR GUIDES</h1>
+		<section class="guides flex flex-justify-space-around pa-3">
+			
+			<div class="text-center pa-2 guide">
+				<div class="guide-image-wrapper no-overflow round center">
+					<img src="/assets/amalie.jpeg" alt="guide picture" class="guide-image full-width">
+				</div>
+				<h2 class="pt-3">Amalie</h2>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam autem rerum totam illum tempore animi quidem, blanditiis magni, itaque officiis cupiditate odit velit? Vel</p>
+			</div>
+			<div class="text-center pa-2 guide">
+				<div class="guide-image-wrapper no-overflow round center">
+					<img src="/assets/christine.jpg" alt="guide picture" class="guide-image full-width">
+				</div>
+				<h2 class="pt-3">Christine</h2>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam autem rerum totam illum tempore animi quidem, blanditiis magni, itaque officiis cupiditate odit velit? Vel</p>
+			</div>
+			<div class="text-center pa-2 guide">
+				<div class="guide-image-wrapper no-overflow round center">
+					<img src="/assets/hanna.jpg" alt="guide picture" class="guide-image full-width">
+				</div>
+				<h2 class="pt-3">Hanna</h2>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam autem rerum totam illum tempore animi quidem, blanditiis magni, itaque officiis cupiditate odit velit? Vel</p>
+			</div>
+		</section>
 	</main>
 	
 	<?php include('partials/footer.php'); ?>
