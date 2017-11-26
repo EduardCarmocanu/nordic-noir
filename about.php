38878848ca955863f7d9eba00921c79e8428e27c
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-26 13:40:55 +0100

    Minor restructuring

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Nordi-Noir About</title>
+	<script src="https://use.fontawesome.com/96a62ba6a5.js"></script>
+	<link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+	<?php include('partials/navbar.php'); ?>
+	
+	<main>
+		<section>BORGEN</section>
+		<section>THE KILLING</section>
+		<section>THE BRIDGE</section>
+		<section>PRIVATE TOURS</section>
+	</main>
+	
+	<?php include('partials/footer.php'); ?>
+</body>
+</html>
