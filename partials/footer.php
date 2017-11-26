c6192ddbd0f473e298a423a8ae78694a8f06a7c7
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-26 13:42:10 +0100

    Moved scripts refferences in the footer partial

---
+++ partials/footer.php
@@ -0,0 +1,38 @@
+<footer class="pt-2 pb-5 main-footer ">
+	<div class="footer-content-wrapper center flex flex-justify-center">
+		<div class="footer-col text-left">
+			<h3>ABOUT US</h3>
+			<br>
+			<p>
+				Nordic Noir Tours offers<br>
+				tours based on<br>
+				Scandinavian crime TV series
+			</p>
+			<br>
+			<h3>FAQ</h3>
+		</div>
+		<div class="footer-col text-center">
+			<h3>FOLLOW US</h3>
+			<br>
+			<div class="flex flex-justify-center">
+				<img src="#" alt="1">
+				<img src="#" alt="2">
+				<img src="#" alt="3">
+			</div>
+			<p>COPYRIGHT 2017 NORDIC NOIR</p>
+		</div>
+		<div class="footer-col text-right">
+			<h3>CONTACT US</h3>
+			<br>
+			<p>
+				Ved Vesterport 2,<br>
+				1612 København V, Danmark<br>
+				+45 31857553<br>
+				info@nordicnoir.dk
+			</p>
+		</div>
+	</div>
+</footer>
+
+<script src="js/util.js"></script>
+<script src="js/main.js"></script>
