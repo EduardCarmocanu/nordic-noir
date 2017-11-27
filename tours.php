35168b52df5a9c763fe5a6dd5b6914c6b204e9e7
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-27 19:08:08 +0100

    Tours Page

---
+++ tours.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Nordic-Noir Tours</title>
+	<script src="https://use.fontawesome.com/96a62ba6a5.js"></script>
+	<link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+	<?php include('partials/navbar.php'); ?>
+	
+	
+	<main>
+		<div class="tours-wrapper">
+			<div class="tour-wrapper no-overflow quarter-view-height full-width text-center has-background white" id="tour-1">
+				<div class="tour-content-wrapper relative full-height full-width">
+					<h2 class="tour-name pt-5">The bridge</h2>	
+				</div>
+			</div>
+			<div class="tour-wrapper no-overflow quarter-view-height full-width text-center has-background white" id="tour-2">
+				<div class="tour-content-wrapper relative full-height full-width">
+					<h2 class="tour-name pt-5">The killing</h2>	
+				</div>
+			</div>
+			<div class="tour-wrapper no-overflow quarter-view-height full-width text-center has-background white" id="tour-3">
+				<div class="tour-content-wrapper relative full-height full-width">
+					<h2 class="tour-name pt-5">Borgen</h2>	
+				</div>
+			</div>
+			<div class="tour-wrapper no-overflow quarter-view-height full-width text-center has-background white" id="tour-4">
+				<div class="tour-content-wrapper relative full-height full-width">
+					<h2 class="tour-name pt-5">Private tours</h2>	
+				</div>
+			</div>
+		</div>		
+	</main>
+	
+	<?php include('partials/footer.php'); ?>
+</body>
+</html>
