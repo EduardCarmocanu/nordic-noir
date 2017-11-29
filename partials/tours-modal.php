75136ba9bad324ee3b5c9519f92b7d2285b36193
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-29 16:44:37 +0100

    Change HTML Structure

--- partials/tours-modal.php
+++ partials/tours-modal.php
@@ -1,18 +1,36 @@
-<div class="absolute" id="tours-modal">
-	<div id="tours-modal-content-wrapper">
-		<div class="tour-details">
+<div class="absolute active center pa-3" id="tours-modal">
+	<div id="tours-modal-content-wrapper flex justify-content-center">
+		<div class="tour-details half-width">
 			<h2>Tour name</h2>
-			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic fugit atque fuga rerum rem, quas excepturi iste tempore aliquid impedit minima quod amet aperiam similique velit omnis officia voluptate molestiae.</p>
+			<p>Tour description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic fugit atque fuga rerum rem, quas excepturi iste tempore aliquid impedit minima quod amet aperiam similique velit omnis officia voluptate molestiae.</p><br>
+			<p>Tour description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic fugit atque fuga rerum rem, quas excepturi iste tempore aliquid impedit minima quod amet aperiam similique velit omnis officia voluptate molestiae.</p>
 			<div class="tour-controls">
 				<div class="number-of-people">
-
+					<div>
+						<p>Number of people</p>
+					</div>
+					<div>
+						<img src="#" alt="" id="quantity-control">
+						<h3>4</h3>
+						<img src="#" alt="" id="quantity-control">
+					</div>
+					
 				</div>
 				<div class="total-price">
-
+					<div>
+						<p>Total price</p>
+					</div>
+					<h3>400 DKK</h3>
+				</div>
+				<div class="book-tours">
+					<a href="#" class="CTA-btn-white">BOOK A TOUR</a>
 				</div>
 			</div>
 		</div>
-		<div class="modal-form">
+		<div class="tour-image half-width">
+			<img src="/assets/2.jpg" alt="">
+		</div>
+		<!-- <div class="modal-form">
 			<form action="#">
 				<div class="form-group">
 					<input type="text" class="input text-input" name="email">
@@ -24,6 +42,6 @@
 					<input type="text" class="input text-input" name="body">
 				</div>
 			</form>
-		</div>
+		</div> -->
 	</div>
 </div>
