77b970ed180f2336dd7261b0d889c7e7a8a77222
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-30 10:06:00 +0100

    Changes strcture to help with usability

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,54 +1,30 @@
 <header>
-    <nav class="flex flex-justify-space-around full-view-width fixed">
+    <nav class="flex flex-justify-space-around flex-align-center full-view-width">
         <section>
             <figure>
                 <a href="/">
-                    <img src="http://nordicnoirtours.com/wp-content/uploads/2014/05/logo.png" alt="Nordic Noir Tours Logo" class="pl-4 full-height" id="logo">
+                    <img src="http://nordicnoirtours.com/wp-content/uploads/2014/05/logo.png" alt="Nordic Noir Tours Logo" class="pa-1 pl-4 full-height" id="logo">
                 </a>
             </figure>
         </section>
         <section class="bigger big-nav full-width full-height">
             <ul class="navbar-items-wrapper flex">
-                <li class="pa-2">
-                    <a href="/">HOME</a>
-                </li>
-                <li class="pa-2">
-                    <a href="/about.php">ABOUT</a>
-                </li>
-                <li class="pa-2">
-                    <a href="/tours.php">TOURS</a>
-                </li>
-                <li class="pa-2">
-                    <a href="/recommandations.php">RECOMMANDATIONS</a>
-                </li>
-                <li class="pa-2">
-                    <a href="/contact.php">CONTACT</a>
-                </li>
+            <a href="/"><li>
+                    HOME
+                </li></a>
+                <a href="/about.php"><li>
+                    ABOUT
+                </li></a>
+                <a href="/tours.php"><li>
+                    TOURS
+                </li></a>
+                <a href="/recommandations.php"><li>
+                    RECOMMANDATIONS
+                </li></a>
+                <a href="/contact.php"><li>
+                    CONTACT
+                </li></a>
             </ul>
         </section>
-
-
-        <!-- 
-            REMOVED FOR TESTING
-         -->
-        <!-- <section class="smaller small-nav">
-            <section class="side-bar full-view-width-height fixed" id="sideNav">
-                <ul class="flex flex-justify-space-between flex-align-center full-height flex-column">
-                    <li>
-                        <a href="/about">ABOUT</a>
-                    </li>
-                    <li>
-                        <a href="/tours">TOURS</a>
-                    </li>
-                    <li>
-                        <a href="/recommandations">RECOMMANDATIONS</a>
-                    </li>
-                    <li>
-                        <a href="/contact">CONTACT</a>
-                    </li>
-                </ul>
-            </section>
-            <i class="fa fa-bars relative" aria-hidden="true" id="hamburger"></i>
-        </section> -->
     </nav>
 </header>
