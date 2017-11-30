933cb15ecaecba0f96f24870f897253d3f04acbf
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-30 10:06:32 +0100

    Completed Modal

--- partials/tours-modal.php
+++ partials/tours-modal.php
@@ -1,47 +1,46 @@
 <div class="absolute active center pa-3" id="tours-modal">
-	<div id="tours-modal-content-wrapper flex justify-content-center">
-		<div class="tour-details half-width">
-			<h2>Tour name</h2>
-			<p>Tour description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic fugit atque fuga rerum rem, quas excepturi iste tempore aliquid impedit minima quod amet aperiam similique velit omnis officia voluptate molestiae.</p><br>
-			<p>Tour description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic fugit atque fuga rerum rem, quas excepturi iste tempore aliquid impedit minima quod amet aperiam similique velit omnis officia voluptate molestiae.</p>
-			<div class="tour-controls">
+	<div class="tour-modal-header flex flex-justify-space-between pb-2">
+		<h2>Tour name</h2>
+		<h3 id="close-modal"><i class="fa fa-times" aria-hidden="true"></i></h3>
+	</div>
+	<div class="tours-modal-content-wrapper flex">
+		<div class="tour-details-wrapper flex flex-column mr-4">
+			<div class="tour-details flex flex-column">
+				<p>Tour description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic fugit atque fuga rerum rem, quas excepturi iste tempore aliquid impedit minima quod amet aperiam similique velit omnis officia voluptate molestiae.</p><br>
+				<p>Tour description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic fugit atque fuga rerum rem, quas excepturi iste tempore aliquid impedit minima quod amet aperiam similique velit omnis officia voluptate molestiae.</p>
+			</div>
+			<div class="tour-controls inline-flex text-center flex-align-center ma-2 mt-5 flex-justify-space-between ">
 				<div class="number-of-people">
 					<div>
 						<p>Number of people</p>
 					</div>
-					<div>
-						<img src="#" alt="" id="quantity-control">
-						<h3>4</h3>
-						<img src="#" alt="" id="quantity-control">
+					<div class="flex flex-justify-space-between">
+						<h2><i class="fa fa-angle-left" aria-hidden="true"></i></h2>
+						<h2 class="paragraph">4</h2>
+						<h2><i class="fa fa-angle-right" aria-hidden="true"></i></h2>
 					</div>
-					
 				</div>
 				<div class="total-price">
 					<div>
 						<p>Total price</p>
 					</div>
-					<h3>400 DKK</h3>
+					<div>
+						<h2 class="paragraph">400 DKK</h2>
+					</div>
+					
 				</div>
 				<div class="book-tours">
-					<a href="#" class="CTA-btn-white">BOOK A TOUR</a>
+					<a href="#" class="CTA-btn-black">BOOK A TOUR</a>
 				</div>
 			</div>
+			<div>
+				<h6>*Tours are free for children under the age of 12</h6>
+				<h6>*If you book before 14:00 pm thursday, each person will receive 50 DKK discount</h6>
+			</div>
 		</div>
-		<div class="tour-image half-width">
+		<div class="tour-image half-width no-overflow">
 			<img src="/assets/2.jpg" alt="">
 		</div>
-		<!-- <div class="modal-form">
-			<form action="#">
-				<div class="form-group">
-					<input type="text" class="input text-input" name="email">
-				</div>
-				<div class="form-group">
-					<input type="text" class="input text-input" name="name">
-				</div>
-				<div class="form-group">
-					<input type="text" class="input text-input" name="body">
-				</div>
-			</form>
-		</div> -->
 	</div>
+	
 </div>
