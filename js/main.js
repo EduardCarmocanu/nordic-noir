b6eaad070c41a5b4438025554ddfcd9643aa35e2
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-11-30 10:05:15 +0100

    implemented modal functionality

--- js/main.js
+++ js/main.js
@@ -1,38 +1,33 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
+    // var triggerPoints = $('*[trigger]');
+    // var nav = $('nav')[0];
     
-    /*
-    * REMOVED FOR TESTING
-    */
-    
-    // var sideShow = false;
-    // $("#hamburger").addEventListener('click', function () {
-    //     if (sideShow){
-    //         $('#sideNav').style.right = '-100vw';
-    //     }else{
-    //         $('#sideNav').style.right = '0vw';
+    // function setNavigationBar() {
+    //     for (var i = 0; i < triggerPoints.length; i++) {
+    //         if (window.pageYOffset > triggerPoints[i].offsetTop) {
+    //             // Sets the value of the trigger attribute to the background color
+    //             nav.style.backgroundColor = triggerPoints[i].getAttribute('trigger');
+    //         }
     //     }
-    //     sideShow = !sideShow;
-    // });
-
-
-    // Check to see if element exists
-
-    /*
-    * Get all the elements that have the 'trigger' attribute
-    */
-    var triggerPoints = $('*[trigger]');
-    var nav = $('nav')[0];
-
-    function setNavigationBar() {
-        for (var i = 0; i < triggerPoints.length; i++) {
-            if (window.pageYOffset > triggerPoints[i].offsetTop) {
-                // Sets the value of the trigger attribute to the background color
-                nav.style.backgroundColor = triggerPoints[i].getAttribute('trigger');
-            }
+    // }
+    // window.addEventListener('scroll', setNavigationBar);
+    
+    var modalActive = false;
+    function toggleTour () {
+        if (!modalActive) {
+            $('#tours-modal').style.display = "block";
         }
+        else {
+            $('#tours-modal').style.display = "none";
+        }
+        modalActive = !modalActive;
     }
-    window.addEventListener('scroll', setNavigationBar);
-
+    
+    var tours = $('.tour');
+    for(var i = 0; i < tours.length; i++) {
+        tours[i].addEventListener('click', toggleTour);
+    }
+    $('#close-modal').addEventListener('click', toggleTour);
 });
 
