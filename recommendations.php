70785ba5031d8af6ba7e40b9c0ed64299ef00f1f
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-03 20:55:36 +0100

    Created recommendations page

---
+++ recommendations.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Nordi-Noir</title>
+	<script src="https://use.fontawesome.com/96a62ba6a5.js"></script>
+	<link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+	<?php include('partials/navbar.php'); ?>	
+	
+	<main>
+		<section class="recommendations-header full-view-width-height has-background">
+			<div class="recommendations-header-wrapper pa-5">
+				<h1>RECOMMENDATIONS</h1>
+				<br>
+				<p>
+					During out tours, we make refferences to many other Nordic Noir films, TV-series and sometimes novels. If they take place in Copenhagen, we show the locations.
+				</p>
+				<br>
+				<p>
+					If you can't get your appetite for Nordic Noir genre satisfied, here are some suggestions (after of course you have see Wallander 2013-2015, SWE and stieg Larsson's Millenniu 2010. SWE)
+				</p>
+				<br>
+				<p>
+					Please take a look at our very subjective list below.	
+				</p>
+			</div>
+		</section>
+		<section class="recommendations-list">
+			<div class="recommendations-list-wrapper">
+				<div class="recommendation pb-5">
+					<h2>Trapped (2015, Ofaerd, ISL)</h2>
+					<br>
+					<p>A murderer on the loose in a little Islandic village and a snow storm as main character.</p>
+					<br>
+					<p>Brilliant performance Olafur Darri Olafsson as the chief of local police and Bjarne Henriksen (Theis Birke Larsen in The Killing) as a shady captain.</p>
+				</div>
+				<div class="recommendation pb-5">
+					<h2>Blue Eyes (2014, Bla Ogon, SWE)</h2>
+					<br>
+					<p>Taking place in Stockholm on the backdrop of the political campaign of a xenophobic party and the violent actions of an extreme right group</p>
+					<br>
+					<p>The guardian: It's as claustrophobic as Homeland and as tightly wound as the Wire - But in today's climate, with the refugee crisis and the rise in nationalism</p>
+					<br>
+					<p>Adam reier Lundgren as the terrifying psychopath Mattias</p>
+				</div>
+				<div class="recommendation pb-5">
+					<h2>Thicker Than Water (2014, Tjockare an vatten, SWE)</h2>
+					<br>
+					<p>A mother who owns a family hotel with one of her sons commits suicide. She leaves one summer to the 3 alienated siblings to run the place together. If profit is being made at the end of the season. They will be able to share the inheritance - otherwise the family fortune will be donated. Old family quarrels are drawn into the plot.</p>
+					<br>
+					<p>Takes place on the beautiful islands of Aland (between Sweeden and Finland)</p>
+				</div>
+			</div>
+		</section>
+	</main>
+	
+	<?php include('partials/footer.php'); ?>
+</body>
+</html>
