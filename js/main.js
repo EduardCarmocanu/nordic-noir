bd4e7517d32c8cb72fef4b30239ef1601539317c
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-03 20:56:05 +0100

    cleaned main js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
-    // var triggerPoints = $('*[trigger]');
-    // var nav = $('nav')[0];
-    
-    // function setNavigationBar() {
-    //     for (var i = 0; i < triggerPoints.length; i++) {
-    //         if (window.pageYOffset > triggerPoints[i].offsetTop) {
-    //             // Sets the value of the trigger attribute to the background color
-    //             nav.style.backgroundColor = triggerPoints[i].getAttribute('trigger');
-    //         }
-    //     }
-    // }
-    // window.addEventListener('scroll', setNavigationBar);
     
     var modalActive = false;
     function toggleTour () {
