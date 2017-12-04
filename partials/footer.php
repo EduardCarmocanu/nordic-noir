a49876a2a0098f84a8ffb057fa633b91958ff0b1
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-04 21:02:17 +0100

    Added linked to footer and removed phone number

--- partials/footer.php
+++ partials/footer.php
@@ -15,11 +15,11 @@
 			<h3>FOLLOW US</h3>
 			<br>
 			<div class="flex flex-justify-center">
-				<a href="#"><img src="/assets/icons/facebook-logo.svg" alt="1"></a>
-				<a href="#"><img src="/assets/icons/twitter-logo-silhouette.svg" alt="2"></a>
-				<a href="#"><img src="/assets/icons/instagram-symbol.svg" alt="3"></a>
+				<a href="https://www.facebook.com/NordicNoirTours"><img src="/assets/icons/facebook-logo-white.png" alt="1"></a>
+				<a href="https://twitter.com/NordicNoirTours"><img src="/assets/icons/twitter-logo-silhouette-white.png" alt="2"></a>
+				<a href="https://www.instagram.com/nordicnoirtours/"><img src="/assets/icons/instagram-symbol-white.png" alt="3"></a>
 			</div>
-			<p class="copyright pt-1">COPYRIGHT 2017 NORDIC NOIR</p>
+			<p class="copyright pt-1">&copy; COPYRIGHT 2017 NORDIC NOIR</p>
 		</div>
 		<div class="footer-col text-right">
 			<h3>CONTACT US</h3>
@@ -27,7 +27,6 @@
 			<p>
 				Ved Vesterport 2,<br>
 				1612 København V, Danmark<br>
-				+45 31857553<br>
 				info@nordicnoir.dk
 			</p>
 		</div>
