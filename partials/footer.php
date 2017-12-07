8478c20ceb2a8db1252b9324e1c4285563dccf36
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-07 20:26:18 +0100

    Removed live reload | It was slowing down loading times

--- partials/footer.php
+++ partials/footer.php
@@ -42,4 +42,4 @@
 <script src="js/mobileNav.js"></script>
 
 
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<!-- <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script> -->
