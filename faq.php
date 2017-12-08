7533c1b90c4d87cd5116bb807f7a0b5eb3a12353
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-08 18:32:56 +0100

    Finished FAQ functionality

--- faq.php
+++ faq.php
@@ -14,7 +14,32 @@
 
 	<main>
 		<div class="faq-container center pa-5">
-			<div class="question no-overflow">
+			<h1 class="pb-5 pt-3">FAQ</h1>
+			<div class="question no-overflow pb-2" id="0">
+				<h3><i class="fa fa-plus" aria-hidden="true"></i> How much does a tour cost?</h3>
+				<br>
+				<p>If there is an open tour in Copenhagen, it costs 150 dkk per person per tour. You receive a 50 dkk discount per person per tour if you make a reservation before Thursday 2 pm.<br>
+				The tours are free for children below the age of 12.<br>
+				<br>
+				If there is no open tour, the fees for private tour apply. Please contact us.<br>
+				<br>
+				The open tours in Malmö on Sundays cost 300 dkk per person.<br>
+				<br>
+				The cost of a private tour depends on the size of the group, the length of the tour and other wishes. Please send us an email and we will get back to you.</p>
+			</div>
+			<div class="question no-overflow pb-2" id="1">
+				<h3><i class="fa fa-plus" aria-hidden="true"></i> How much does a tour cost?</h3>
+				<br>
+				<p>If there is an open tour in Copenhagen, it costs 150 dkk per person per tour. You receive a 50 dkk discount per person per tour if you make a reservation before Thursday 2 pm.<br>
+				The tours are free for children below the age of 12.<br>
+				<br>
+				If there is no open tour, the fees for private tour apply. Please contact us.<br>
+				<br>
+				The open tours in Malmö on Sundays cost 300 dkk per person.<br>
+				<br>
+				The cost of a private tour depends on the size of the group, the length of the tour and other wishes. Please send us an email and we will get back to you.</p>
+			</div>
+			<div class="question no-overflow pb-2" id="2">
 				<h3><i class="fa fa-plus" aria-hidden="true"></i> How much does a tour cost?</h3>
 				<br>
 				<p>If there is an open tour in Copenhagen, it costs 150 dkk per person per tour. You receive a 50 dkk discount per person per tour if you make a reservation before Thursday 2 pm.<br>
--- js/faq.js
+++ js/faq.js
@@ -2,7 +2,17 @@
 
 var questions = $('.question');
 for (var i = 0; i < questions.length; i++) {
+	// Binds function to click event on questions
 	questions[i].addEventListener('click', function () {
+
+		// Toggles class
 		this.classList.toggle('question-active');
+
+		// Changes the font awesome icon to either a minus or a plus
+		if (this.classList.contains('question-active')) {
+			this.children[0].children[0].classList = "fa fa-minus";
+		}else {
+			this.children[0].children[0].classList = "fa fa-plus";
+		}
 	});
 }
