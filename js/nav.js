1f7a205f714edfd1b50549d5374ec97f1d568be9
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-08 19:46:23 +0100

    Changed script to run when the DOM is loaded and commented code

--- js/nav.js
+++ js/nav.js
@@ -1,24 +1,33 @@
 'use strict';
 
+// Runs script on the DOM is loaded
+document.addEventListener('DOMContentLoaded', function () {
 
+	// Removes predefined component class "active" and adds initial
+	$('nav')[0].classList.remove('active');
+	$('nav')[0].classList.add('initial');
 
-if (window.innerWidth > 768) {
-
-
-var triggerPoint = $('section[trigger]')[0];
-function setNavigationBar() {
-	
-	if (window.pageYOffset > triggerPoint.offsetTop - 50) {
-		$('nav')[0].classList.add('active');
-		$('nav')[0].classList.remove('initial');
+	// Checks the window width in order to avoid conflicts with mobileNav.js
+	if (window.innerWidth > 768) {
+		
+		// Stored the elements that have the "trigger" attribute
+		var triggerPoint = $('section[trigger]')[0];
+		function setNavigationBar() {
+			
+			// Checks to see if windows reached trigger point
+			if (window.pageYOffset > triggerPoint.offsetTop - 50) {
+				$('nav')[0].classList.add('active');
+				$('nav')[0].classList.remove('initial');
+			}
+			else {
+				$('nav')[0].classList.remove('active');
+				$('nav')[0].classList.add('initial');
+			}
+		}
+		// Binds function setNavigationBar to the window scroll event
+		window.addEventListener('scroll', setNavigationBar);
+		
 	}
-	else {
-		$('nav')[0].classList.remove('active');
-		$('nav')[0].classList.add('initial');
-	}
-}
-window.addEventListener('scroll', setNavigationBar);
+});
 
-}else{
 
-}
