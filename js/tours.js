235a0ec727c62f39919b3964763083213db856fe
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-08 18:33:49 +0100

    Comented code and renamed numberOfPeople variable

--- js/tours.js
+++ js/tours.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
     
     var toursData;
     var currentTourPrice = 0;
-    var numberOfPeople = 1;
+    var currentNumberOfPeople = 1;
     var tour;
     
     
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            
             toursData = JSON.parse(this.responseText);
+            
             setUpTours();
             
         }
@@ -27,61 +27,88 @@ document.addEventListener("DOMContentLoaded", function() {
         var tourNames = $('.tour-name');
 
         for(var i = 0; i < tours.length; i++) {
+
+            // Binds function to each tour
             tours[i].addEventListener('click', openTour);
 
+            // Checks if tours data and image url exists
             if (toursData[i] && toursData[i].hasOwnProperty('image_url')) {
+
+                // Formats the url
                 toursData[i].image_url = toursData[i].image_url.replace('../', "");
+
+                // Sets up the tour background image
                 tours[i].style.backgroundImage = "url(" + toursData[i].image_url + ")"
+                // Sets up the tours title
                 tourNames[i].innerHTML = toursData[i].title;
             }
             
             
         }
         
+        // Binds function to close button
         $('#close-modal').addEventListener('click', function () {
+            // Hides the modal
             $('#tours-modal').style.display = "none";
         });
+
+        // Binds price incrementation and decrementation functions to buttons
         $('#decrement').addEventListener('click', decrement);
         $('#increment').addEventListener('click', increment);
     }
 
     function openTour (el) {
         
+        // Gets the data for the clicked tour
         tour = toursData[parseInt(el.target.getAttribute('id'))];
+        // Sets initial price for the tour
         currentTourPrice = parseInt(tour.price);
-        numberOfPeople = 1;
+        // Sets the intial number of people
+        currentNumberOfPeople = 1;
         
+        // Replaces new lines with <br> tags
         tour.content = tour.content.replace(/(\n)+/g, '<br />');
         tour.aditional_info = tour.aditional_info.replace(/(\n)+/g, '<br />');
+
+        // Formats the url to math the path of the image
         tour.thumbnail_url = tour.thumbnail_url.replace('../', '');
         
+
+        // Displays data on the modal
         $('#tour-content p').innerHTML = tour.content;
         $('#tour-title').innerHTML = tour.title;
         $('#additional-info h6').innerHTML = tour.aditional_info;
         $('#tour-image').innerHTML = "<img src=" + tour.thumbnail_url + ">"
         $('#total-price').innerHTML = currentTourPrice + ' DKK';
-        $('#number-of-people').innerHTML = numberOfPeople;
+        $('#number-of-people').innerHTML = currentNumberOfPeople;
         
+        // Displays the modal
         $('#tours-modal').style.display = "block";
     }
     function increment() {
-        if (numberOfPeople < 30) {
+        if (currentNumberOfPeople < 30) {
             
+            // Adds the base tours price per person to the current tours price
             currentTourPrice += parseInt(tour.price);
-            numberOfPeople++;
+            // Increments the number of people by 1
+            currentNumberOfPeople++;
             
+            // Displays the updated data
             $('#total-price').innerHTML = currentTourPrice + ' DKK';
-            $('#number-of-people').innerHTML = numberOfPeople;
+            $('#number-of-people').innerHTML = currentNumberOfPeople;
         }
     }
     function decrement () {
-        if (numberOfPeople > 1) {
+        if (currentNumberOfPeople > 1) {
             
+            // Substracts the base tours price per person from the current tours price
             currentTourPrice -= tour.price;
-            numberOfPeople--;
+            // Decrements the current number of people by 1
+            currentNumberOfPeople--;
             
+            // Displays the updated data
             $('#total-price').innerHTML = currentTourPrice + ' DKK';
-            $('#number-of-people').innerHTML = numberOfPeople;
+            $('#number-of-people').innerHTML = currentNumberOfPeople;
         }
     }
    
