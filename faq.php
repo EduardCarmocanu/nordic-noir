43473cc399aed91812cdb370d3a40a068961c50d
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-09 23:25:24 +0100

    Disabled zooming on mobile devices

--- faq.php
+++ faq.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Nordi-Noir</title>
 	<script src="https://use.fontawesome.com/96a62ba6a5.js"></script>
