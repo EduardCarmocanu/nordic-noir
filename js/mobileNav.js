70190332e312a95e563c981b7995150bb7bb570a
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-09 20:09:47 +0100

    refactored mobileNav.js

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,23 +1,8 @@
-var smallNavOpen = false;
 var hamburgers = $('.hamburger');
 
 for(i = 0;i<hamburgers.length; i++){
-    hamburgers[i].addEventListener('click',changeSmallNav);
-}
-
-
-function changeSmallNav(){
-	if(smallNavOpen){
-		$('#smallNav').classList.remove('navOpen');
-		$('#smallNav').classList.add('navClosed');
-	}else{
-		$('#smallNav').classList.remove('navClosed');        
-		$('#smallNav').classList.add('navOpen');        
-	}
-	smallNavOpen = !smallNavOpen;
-	console.log("ASDASD");
-}
-
-if (window.innerWidth < 768) {
-	window.addEventListener('click', function () {});
+	hamburgers[i].addEventListener('click', function () {
+		$('#smallNav').classList.toggle('navOpen');
+		$('#smallNav').classList.toggle('navClosed');
+	});
 }
