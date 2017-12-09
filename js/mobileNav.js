8e865fd3f11403ecda1f47d0f8c5d814927ac155
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-09 20:33:18 +0100

    implemented mobile nav toggling functionality

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,5 +1,16 @@
-var hamburgers = $('.hamburger');
 
+var mobileTriggers = $("*[mobileTrigger]");
+
+window.addEventListener('scroll', function () {
+	for (var i = 0; i < mobileTriggers.length; i++) {
+		if (window.pageYOffset > mobileTriggers[i].offsetTop - 50) {
+			$("#hamburger").style.color = mobileTriggers[i].getAttribute('mobileTrigger');
+		}
+	}
+});
+
+
+var hamburgers = $('.hamburger');
 for(i = 0;i<hamburgers.length; i++){
 	hamburgers[i].addEventListener('click', function () {
 		$('#smallNav').classList.toggle('navOpen');
