f588638a582bfd33eecc2b72175160370610ca85
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-10 15:25:09 +0100

    deleted console.logs

--- js/util.js
+++ js/util.js
@@ -1,9 +1,11 @@
 // Shorthand DOM element selector function
 function $(selector) {
 	if (selector[0] === '#') {
-		return document.querySelector(selector)[0];
+		
+		return document.querySelector(selector);
 	}
 	return document.querySelectorAll(selector);
+
 }
 
 // Shortfunction for loggin data to the console
