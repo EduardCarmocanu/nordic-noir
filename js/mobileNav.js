3eb20f04a6ee859fa454d249773f6a5ac584980a
Eduard Carmocanu <carmocanueduard@gmail.com>
2017-12-11 11:10:19 +0100

    Rewrote mobileNav.js

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,26 +1,31 @@
 "use strict";
 
-// Gets all triggers
-var mobileTriggers = $("*[mobileTrigger]");
-
-// Binds function to the scroll event on the window object
-window.addEventListener('scroll', function () {
-
-	// Runs through all the triggers
-	for (var i = 0; i < mobileTriggers.length; i++) {
-		if (window.pageYOffset > mobileTriggers[i].offsetTop - 50) {
-			// Sets the color of the hamburger with the attribute data
-			$(".hamburger")[0].style.color = mobileTriggers[i].getAttribute('mobileTrigger');
+if (window.innerWidth < 768) {
+	$('nav')[0].classList.remove('active');
+	$('nav')[0].classList.add('initial');
+	
+	if (window.location.pathname === '/' || window.location.pathname === '/index.php') {
+		function mobileNav() {
+			var triggerPoints = $('*[mobileTrigger]');
+			
+			for (var i = 0; i < triggerPoints.length; i++) {
+				if (window.pageYOffset > triggerPoints[i].offsetTop - 50) {
+					$('.hamburger')[0].style.color = triggerPoints[i].getAttribute('mobileTrigger');
+				}
+			}
 		}
+		window.addEventListener('scroll', mobileNav);
 	}
-});
+	else {
+		$('nav')[0].style.backgroundColor = "#f8f8f8";
+		$('.hamburger')[0].style.color = "#171717";
+	}
 
-// gets the hamburger elements
+}
+
 var hamburgers = $('.hamburger');
-// runs through all of them and bind anonymous function to their click event
 for(var i = 0; i < hamburgers.length; i++){
 	hamburgers[i].addEventListener('click', function () {
-		// toggles functions needed for the right display
 		$('.small-nav')[0].classList.toggle('navOpen');
 		$('.small-nav')[0].classList.toggle('navClosed');
 	});
